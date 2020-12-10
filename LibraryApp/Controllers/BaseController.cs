dc6b60e2df38f01be42c2f65e5991da7fef0b663
zagar_zg <zagar.zg@gmail.com>
2020-12-10 12:16:00 +0100

    Add BaseController and HealthCheckController

---
+++ LibraryApp/Controllers/BaseController.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace LibraryApp.Controllers
+{
+    [ApiController]
+    [Route("[controller]/[action]")]
+    public abstract class BaseController : ControllerBase
+    {
+    }
+}
