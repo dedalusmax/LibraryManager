30c254ac3ed4bf3055227c2a142d1c8794de8f04
zagar_zg <zagar.zg@gmail.com>
2020-12-10 13:55:01 +0100

    Add Models

---
+++ LibraryApp.DomainLayer/Entities/Book.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LibraryApp.DomainLayer.Entities
+{
+	public class Book : BaseEntity
+	{
+		public string Title { get; set; }
+		public string Author { get; set; }
+		public string Publisher { get; set; }
+		public DateTime DateOfPublication { get; set; }
+	}
+}
---
+++ LibraryApp.DomainLayer/Interfaces/IEntity.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LibraryApp.DomainLayer.Interfaces
+{
+	public interface IEntity
+	{
+		Guid Id { get; set; }
+	}
+}
