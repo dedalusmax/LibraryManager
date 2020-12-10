d357dfbf4dad292f461ba97824cb4f00151adebc
zagar_zg <zagar.zg@gmail.com>
2020-12-10 15:37:44 +0100

    Implement service and register it via ServiceCollection pattern

---
+++ LibraryApp.BusinessLayer/Interfaces/IBookService.cs
@@ -0,0 +1,17 @@
+﻿using LibraryApp.DomainLayer.Entities;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LibraryApp.BusinessLayer.Interfaces
+{
+	public interface IBookService
+	{
+		Task<IEnumerable<Book>> GetAll();
+		Task<Book> Get(Guid id);
+		Task<Book> Create(Book book);
+		Task Update(Book book);
+		Task Delete(Guid id);
+	}
+}
